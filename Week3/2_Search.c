d0958a312309c7d7ae6d9ec960f1eb54984906f5
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-16 23:10:27 +0300

    Fix bags

--- Week3/2_Search.c
+++ Week3/2_Search.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
-void randomFilling(int *array, int lenght)
+#define INTERVAL 100
+
+void randomFilling(int *const array, const size_t length)
 {
-    for (int i = 0; i < lenght; i++)
+    srand(time(NULL));
+    for (size_t i = 0; i < length; ++i)
     {
-        array[i] = 80 + rand() % 10;
+        array[i] = rand() % INTERVAL;
     }
 }
 
-void printing(int *array, int lenght)
+void printArray(int const *const array, const size_t length)
 {
-    for (int i = 0; i < lenght; i++)
+    for (size_t i = 0; i < length; ++i)
         printf("%d ", array[i]);
     printf("\n\n");
 }
@@ -22,50 +27,61 @@ int compare(const void *i, const void *j)
     return *(int *)i - *(int *)j;
 }
 
-void binarySearch(int number, int *array, int lenght)
+void binarySearch(const size_t number, int const *const array, const size_t length)
 {
     int left = 0;
-    int right = lenght - 1;
+    int right = length - 1;
+    bool find = false;
 
     while (left <= right)
     {
         int middle = (left + right) / 2;
         if (number < array[middle])
+        {
             right = middle - 1;
+        }
         else if (number > array[middle])
+        {
             left = middle + 1;
+        }
         else
+        {
             printf("The number %d is in the array\n", number);
-        break;
+            find = true;
+            break;
+        }
     }
-    printf("The number %d is not in the array\n", number);
+    if (!find)
+    {
+        printf("The number %d is not in the array\n", number);
+    }
 }
 
 int main(void)
 {
-    int number = 0;
-    int lenght = 0;
+    const size_t number = 0;
+    const size_t length = 0;
 
-    printf("Enter lenght of array and number of numbers: ");
-    scanf("%d %d", &lenght, &number);
+    printf("Enter length of array and number of numbers: ");
+    scanf("%d %d", &length, &number);
 
-    int *array = calloc(lenght, sizeof(int));
-    int *numbersArray = calloc(number, sizeof(int));
+    int *array = (int *)calloc(length, sizeof(int));
+    int *numbersArray = (int *)calloc(number, sizeof(int));
 
-    randomFilling(array, lenght);
+    randomFilling(array, length);
     randomFilling(numbersArray, number);
 
-    qsort(array, lenght, sizeof(int), compare);
+    qsort(array, length, sizeof(int), compare);
 
     printf("\nArray: ");
-    printing(numbersArray, number);
+    printArray(array, length);
 
     printf("Numbers: ");
-    printing(array, lenght);
+    printArray(numbersArray, number);
 
     for (int i = 0; i < number; ++i)
     {
-        binarySearch(numbersArray[i], array, lenght);
+        binarySearch(numbersArray[i], array, length);
     }
 
     free(array);
