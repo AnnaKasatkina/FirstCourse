814a8922ec3898c4a51634b6fc06085d0cd09362
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-22 20:37:24 +0300

    Add the task

--- Week5/Stack/BalanceBrackets.c
+++ Week5/Stack/BalanceBrackets.c
@@ -1,17 +1,47 @@
 #include "Stack.h"
-
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LEN 256
+
+bool isBracketsCorrect(char *string)
+{
+	struct Stack* brackets = NULL;
+	char open[] = "{([";
+	char close[] = "})]";
+	for (char* character = string; *character != '\0'; ++character) {
+		if (strchr(open, *character) != NULL) {
+			push(&brackets, *character);
+		}
+		if (strchr(close, *character) != NULL)
+		{
+			if (strchr(close, *character) - close != strchr(open, pop(&brackets)) - open)
+				return false;
+		}
+	}
+	return true;
+}
+
+void printResult(bool result)
+{
+	if (result)
+	{
+		printf("Correct");
+	}
+	else
+	{
+		printf("Incorrect");
+	}
+}
 
 int main(void)
 {
-	struct Stack* head = NULL;
+	char string[LEN] = "";
+	gets(string);
 
-	ErrorCode rezult = push(&head, 5);
-	if (rezult != ok)
-	{
-		printf("Stack if empty!");
-	}
-	printf("%d ", top(head, &rezult));
+	bool result = isBracketsCorrect(string);
+	printResult(result);
+	
 	return 0;
-
 }
