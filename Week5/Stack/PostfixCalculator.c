d87e205267c35a17e2ea2d7b0ac4ef9cc89383fe
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-22 18:40:10 +0300

    Add the task

--- Week5/Stack/PostfixCalculator.c
+++ Week5/Stack/PostfixCalculator.c
@@ -1,17 +1,55 @@
 #include "Stack.h"
-
+#include <string.h>
 #include <stdio.h>
+
+#define LEN 256
+
+void removeSpaces(char* string)
+{
+    char* character = string;
+    do
+    {
+        while (*character == ' ')
+        {
+            ++character;
+        }
+    } while (*string++ = *character++);
+}
 
 int main(void)
 {
-	struct Stack* head = NULL;
+    Stack* digits = NULL;
+    char string[LEN] = "";
+    gets(string);
+    removeSpaces(string);
+    for (char* character = string; *character != '\0'; ++character)
+    {
+        if (isdigit(*character))
+        {
+            push(digits, (int)*character - '0');
+        }
+        else
+        {
+            switch (*character)
+            {
+            case '+':
+                push(digits, pop(digits) + pop(digits));
+                break;
+            case '/':
+                push(digits, pop(digits) / pop(digits));
+                break;
+            case '*':
+                push(digits, pop(digits) * pop(digits));
+                break;
+            case '-':
+                push(digits, pop(digits) - pop(digits));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    printf("%d", pop(digits));
 
-	ErrorCode rezult = push(&head, 5);
-	if (rezult != ok)
-	{
-		printf("Stack if empty!");
-	}
-	printf("%d ", top(head, &rezult));
-	return 0;
-
+    return 0;
 }
