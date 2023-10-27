3782d89f25dab6210f808b1f7331610cdc43ffa8
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-27 13:31:19 +0300

    Fix the bag

--- Week5/Stack/Stack.h
+++ Week5/Stack/Stack.h
@@ -13,8 +13,8 @@ typedef struct Stack Stack;
 // Add element in Stack
 ErrorCode push(Stack** head, int value);
 
-// Delete and return top element from Stack
-int pop(Stack** head);
+// Delete top element from Stack
+ErrorCode pop(Stack** head);
 
 // Delete Stack
 ErrorCode freeStack(Stack** head);
