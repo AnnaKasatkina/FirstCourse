99fd05c29be10959b6eca564e0da98397153e059
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-31 00:35:48 +0300

    Fix bags

--- Week5/Stack/Stack.c
+++ Week5/Stack/Stack.c
@@ -5,59 +5,53 @@
 
 struct Stack
 {
-	int value;
-	Stack* previous;
+    int value;
+    Stack* previous;
 };
 
 ErrorCode push(Stack** head, int value)
 {
-	Stack* next = (int *)malloc(sizeof(Stack));
-	if (next == NULL)
-	{
-		return outOfMemory;
-	}
-	next->value = value;
-	next->previous = *head;
-	*head = next;
+    Stack* next = (Stack*)malloc(sizeof(Stack));
+    if (next == NULL)
+    {
+        return outOfMemory;
+    }
+    next->value = value;
+    next->previous = *head;
+    *head = next;
 
-	return ok;
+    return ok;
 }
 
-ErrorCode pop(Stack** head)
+void pop(Stack** head)
 {
-	if (*head == NULL)
-	{
-		return stackIsEmpty;
-	}
+    if (*head == NULL)
+    {
+        return;
+    }
 
-	Stack* trash = *head;
-	*head = (*head)->previous;
-	free(trash);
-
-	return ok;
+    Stack* trash = *head;
+    *head = (*head)->previous;
+    free(trash);
 }
 
 ErrorCode freeStack(Stack** head)
 {
-	Stack* next = (*head)->previous;
-	free(*head);
-	while (next != NULL)
-	{
-		pop(&next);
-	}
-	*head = next;
-
-	return ok;
+    while (*head != NULL)
+    {
+        pop(head);
+    }
+    return ok;
 }
 
 int top(Stack* head, ErrorCode* errorCode)
 {
-	if (head == NULL)
-	{
-		*errorCode = stackIsEmpty;
-		return 0;
-	}
+    if (head == NULL)
+    {
+        *errorCode = stackIsEmpty;
+        return 0;
+    }
 
-	*errorCode = ok;
-	return head->value;
+    *errorCode = ok;
+    return head->value;
 }
