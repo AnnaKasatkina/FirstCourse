0cf9572acb9e34108a59a73ab4d5ab35b45beb27
AnnaKasatkina <akasatkina904@gmail.com>
2023-12-09 00:51:03 +0300

    Fix bags

---
+++ Week6/List/CyclicList/CyclicList.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "List.h"
+
+// Fill the List with indexes
+void fillList(const size_t amount, List* const list, ErrorCode* errorCode);
+
+// Calculate a safe place
+size_t kill(const size_t amount, const size_t gap, List* const list);
---
+++ Week6/List/CyclicList/List.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef enum ErrorCode
+{
+    outOfMemory,
+    ok
+}ErrorCode;
+
+// List element
+typedef struct ListElement
+{
+    size_t value;
+    struct ListElement* next;
+} ListElement;
+
+// Descripter of List
+typedef struct List
+{
+    ListElement* begin;
+    ListElement* end;
+    size_t size;
+} List;
+
+// Return the element that stands index positions apart
+ListElement* getSubsequentElement(ListElement* currentElement, size_t const index);
+
+// Erase next element
+void eraseNextElement(ListElement* const currentElement, List* const list);
+
+// Delete the List
+void freeList(List* list);
+
+// Add the element to the end
+void pushBack(List* const list, const size_t value, ErrorCode* errorCode);
