d38ecb47d4210b5afe955c70315417848b343247
AnnaKasatkina <akasatkina904@gmail.com>
2023-12-27 13:54:34 +0300

    Add task

---
+++ Test2_2/Task2/BinaryTree.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef enum ErrorCode
+{
+    ok,
+    stackIsEmpty,
+    outOfMemory,
+    error,
+    inputError
+}ErrorCode;
+
+typedef struct Node
+{
+    int value;
+    struct Node* leftChild;
+    struct Node* rightChild;
+} Node;
+
+// Add element in tree
+void addElement(Node** const tree, const int value);
+
+// Delete tree
+void deleteTree(Node** const tree);
+
+// Order tree
+void orderTree(Node* tree, int* const array, size_t* index);
+
+// Make tree from array
+Node* makeTreeFromArray(int* array, size_t length);
---
+++ Test2_2/Task2/Main.c
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "Tests.h"
+
+#define ERROR -1
+#define ERROR_STRING "Error!"
+
+bool scanfArray(int* const array, const size_t length)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        if (scanf("%d", &array[i]) != 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int* getArray(size_t* const length)
+{
+    printf("Enter length of array: ");
+    if (scanf("%zu", length) != 1)
+    {
+        return NULL;
+    }
+
+    int* array = (int*)calloc(*length, sizeof(int));
+    if (array == NULL)
+    {
+        return NULL;
+    }
+
+    printf("Enter array: ");
+    if (!scanfArray(array, *length))
+    {
+        free(array);
+        return NULL;
+    }
+    return array;
+}
+
+void printArray(const int* const array, const size_t length)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    if (!testsResult())
+    {
+        printf(ERROR_STRING);
+        return ERROR;
+    }
+
+    size_t length = 0;
+    int* array = getArray(&length);
+    if (array == NULL)
+    {
+        printf(ERROR_STRING);
+        return ERROR;
+    }
+
+    Node* tree = makeTreeFromArray(array, length);
+    if (tree == NULL)
+    {
+        printf(ERROR_STRING);
+        return ERROR;
+    }
+
+    size_t index = 0;
+    orderTree(tree, array, &index);
+
+    printf("Sorted array: ");
+    printArray(array, length);
+
+    free(array);
+    deleteTree(&tree);
+    return 0;
+}
