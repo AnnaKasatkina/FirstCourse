fa9894ba1c2e398c7d4c8551cfa34a96d9645c4f
AnnaKasatkina <akasatkina904@gmail.com>
2024-03-05 18:26:53 +0300

    Fix bags

--- SecondSemester/BWT/BWT.cs
+++ SecondSemester/BWT/BWT.cs
@@ -1,4 +1,6 @@
-﻿public class BWT
+﻿using Microsoft.VisualBasic;
+
+public class BWT
 {
     public static Tuple<string, int> BWTransform(string inputString)
     {
@@ -23,32 +25,43 @@
 
     public static string InverseBWTransform(string bwtString, int endPosition)
     {
-        var countArray = new int[256];
+        var countDictionary = new Dictionary<char, int>();
         var firstCountArray = new int[bwtString.Length];
+
         for (int i = 0; i < bwtString.Length; i++)
         {
-            firstCountArray[i] = countArray[bwtString[i]]++;
+            if (countDictionary.TryGetValue(bwtString[i], out var count))
+            {
+                countDictionary[bwtString[i]] = count + 1;
+            }
+            else
+            {
+                countDictionary[bwtString[i]] = 1;
+            }
+            firstCountArray[i] = count;
         }
 
         int countSmaller = 0;
-        var secondCountArray = new int[256];
-        for (int i = 0; i < secondCountArray.Length; i++)
+        var secondCountDictionary = new Dictionary<char, int>();
+
+        foreach (var kvp in countDictionary.OrderBy(entry => entry.Key))
         {
-            countSmaller += countArray[i];
-            secondCountArray[i] = countSmaller - countArray[i];
+            countSmaller += kvp.Value;
+            secondCountDictionary[kvp.Key] = countSmaller - kvp.Value;
         }
 
         var result = new char[bwtString.Length];
         int index = endPosition;
+
         for (int i = bwtString.Length - 1; i >= 0; i--)
         {
             result[i] = bwtString[index];
-            index = firstCountArray[index] + secondCountArray[bwtString[index]];
+            index = firstCountArray[index] + (countDictionary.TryGetValue(bwtString[index], out var count) ? 
+                secondCountDictionary[bwtString[index]] : 0);
         }
 
         return new string(result);
     }
-
     static int CompareSubstrings(string str, int index1, int index2)
     {
         string substring1 = $"{str[index1..]}{str[..index1]}";
