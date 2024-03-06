836beb8af103df66559eb0fae3b773051853df28
AnnaKasatkina <akasatkina904@gmail.com>
2024-03-06 14:27:55 +0300

    Add task

---
+++ SecondSemester/LZW/LZW/Program.cs
@@ -0,0 +1,30 @@
+﻿public class LZW
+{
+    public static void Compress(string inputFilePath, string outputFilePath)
+    {
+
+    }
+
+    public static void Decompress(string inputFilePath, string outputFilePath)
+    {
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: LZWCompressionApp.exe <file_path> <-c or -u>");
+            return;
+        }
+
+        string filePath = args[0];
+        string key = args[1];
+
+        LZW lzw = new LZW();
+        //lzw.Run(filePath, key);
+    }
+}
