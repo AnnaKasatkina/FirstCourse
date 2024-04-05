80094b2a4f76b8fa9db550cc3a92e36e9d5b17c0
AnnaKasatkina <akasatkina904@gmail.com>
2024-04-05 23:37:40 +0300

    Add base of task

---
+++ SecondSemester/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,78 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        private bool isNewNumber = true;
+
+        private string currentNumber = string.Empty;
+        private int currentValue = 0;
+        private int storedValue = 0;
+        private char currentOperator = ' ';
+
+        public int CurrentValue => currentValue;
+        public char CurrentOperator => currentOperator;
+
+        public void InputNumber(char newNumber)
+        {
+            if (isNewNumber)
+            {
+                currentNumber = string.Empty;
+                isNewNumber = false;
+            }
+            currentNumber += newNumber;
+        }
+
+        public void InputOperator(char newOperator)
+        {
+            isNewNumber = true;
+            if (!int.TryParse(currentNumber, out currentValue))
+            {
+                throw new ArgumentException("Invalid input format", nameof(currentNumber));
+            }
+
+            CalculateResult();
+
+            if (newOperator != '=')
+            {
+                currentOperator = newOperator;
+            }
+        }
+
+        public void CalculateResult()
+        {
+            switch (currentOperator)
+            {
+                case '+':
+                    currentValue = storedValue + currentValue;
+                    break;
+                case '-':
+                    currentValue = storedValue - currentValue;
+                    break;
+                case '×':
+                    currentValue = storedValue * currentValue;
+                    break;
+                case '÷':
+                    if (currentValue != 0)
+                    {
+                        currentValue = storedValue / currentValue;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Error: Division by zero");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            storedValue = currentValue;
+        }
+
+        public void Clear()
+        {
+            currentValue = 0;
+            storedValue = 0;
+            currentOperator = ' ';
+        }
+    }
+}
---
+++ SecondSemester/Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,48 @@
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private Calculator calculator = new Calculator();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textOutput.AppendText(button.Text);
+
+            calculator.InputNumber(button.Text[0]);
+        }
+
+        private void OperationButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (calculator.CurrentOperator != ' ')
+            {
+                calculator.InputOperator('=');
+                textOutput.Text = calculator.CurrentValue.ToString();
+            }
+
+            calculator.InputOperator(button.Text[0]);
+            textOutput.AppendText(button.Text);
+        }
+
+        private void EqualsButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            calculator.InputOperator(button.Text[0]);
+
+            textOutput.Text = calculator.CurrentValue.ToString();
+        }
+
+        private void ClearButtonClick(object sender, EventArgs e)
+        {
+            textOutput.Clear();
+            calculator.Clear();
+        }
+    }
+}
