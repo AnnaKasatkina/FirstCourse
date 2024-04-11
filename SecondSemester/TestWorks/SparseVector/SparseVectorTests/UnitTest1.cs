c41020f4899001fc567d0ed0576f318f49e3e362
AnnaKasatkina <akasatkina904@gmail.com>
2024-04-11 20:10:13 +0300

    Fix test

--- SecondSemester/TestWorks/SparseVector/SparseVectorTests/UnitTest1.cs
+++ SecondSemester/TestWorks/SparseVector/SparseVectorTests/UnitTest1.cs
@@ -37,24 +37,4 @@ public class Tests
 
         Assert.That(result, Is.EqualTo(3));
     }
-    
-    [Test]
-    public void TestAdd()
-    {
-        var vector1 = new SparseVector.SparseVector(5);
-        vector1.AddElement(0, 2);
-        vector1.AddElement(1, 3);
-
-        var vector2 = new SparseVector.SparseVector(5);
-        vector2.AddElement(1, 1);
-        vector2.AddElement(2, 4);
-
-        var result = vector1.Add(vector2);
-
-        Assert.That(result.Elements[0], Is.EqualTo(2));
-        Assert.That(result.Elements[1], Is.EqualTo(4));
-        Assert.That(result.Elements[2], Is.EqualTo(4));
-        Assert.That(result.Elements[3], Is.EqualTo(0));
-        Assert.That(result.Elements[4], Is.EqualTo(0));
-    }
 }
