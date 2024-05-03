f150404d218df3a6e94658411920287fc1f5ebaa
AnnaKasatkina <akasatkina904@gmail.com>
2024-05-03 17:32:09 +0300

    Add task and tests

--- SecondSemester/SkipList/SkipList/Program.cs
+++ SecondSemester/SkipList/SkipList/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SkipList;
 
-Console.WriteLine("Hello, World!");
+SkipList<int> skipList = new SkipList<int>();
+
+skipList.Add(10);
+skipList.Add(5);
+skipList.Add(20);
+skipList.Add(15);
+
+Console.WriteLine("Elements in the SkipList:");
+foreach (var element in skipList)
+{
+    Console.WriteLine(element);
+}
